4af452872cd87ff33e0b6f2550849c37f3da24ee
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-04-13 11:15:59 -0400

    added bundle information

--- app/src/main/java/edu/quinnipiac/hackathon/api/Prompt.kt
+++ app/src/main/java/edu/quinnipiac/hackathon/api/Prompt.kt
@@ -1,12 +1,21 @@
 package edu.quinnipiac.hackathon.api
 
+import android.os.Bundle
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
 
 class Prompt {
-    fun BuildPrompt() {
-        val model = GenerativeModel(
-            modelName = "gemini-pro",
-            apiKey =
-        )
+
+    companion object {
+        val BUNDLE_API = "GEMINI_API_KEY";
+        val BUNDLE_NAME = "CONTACT_NAME";
+        val BUNDLE_RELATION = "CONTACT_RELATION";
+        val BUNDLE_LAST_CONTACT_DATE = "LAST_CONTACT_DATE";
+        val BUNDLE_LAST_CONTACT_CONTEXT = "LAST_CONTACT_CONTEXT";
+        val BUNDLE_REASON = "REASON";
+    }
+
+    suspend fun BuildPrompt(bundle: Bundle) {
+
     }
 }
