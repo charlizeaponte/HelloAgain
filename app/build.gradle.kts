683691d1aa6a46e089bde7ec6c2b2a93e1c7a235
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-04-13 10:54:55 -0400

    update scripts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.ai.client.generativeai:generativeai:0.3.0")
+
 }
---
+++ app/src/main/java/edu/quinnipiac/hackathon/api/Prompt.kt
@@ -0,0 +1,12 @@
+package edu.quinnipiac.hackathon.api
+
+import com.google.ai.client.generativeai.GenerativeModel
+
+class Prompt {
+    fun BuildPrompt() {
+        val model = GenerativeModel(
+            modelName = "gemini-pro",
+            apiKey =
+        )
+    }
+}
